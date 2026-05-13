1c6a29f9e844f0140310a7c581dc53209eebf719
Alexandria Braniff <alexandriaBraniff@gmail.com>
2026-05-13 06:56:44 -0700

    Banner: fix em dash in creator credit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <div className="bg-[#2A5240] text-white text-center text-xs py-2 px-4">
-          Website created by <strong>Alexandria Braniff</strong> &mdash;{" "}
+          Website created by <strong>Alexandria Braniff</strong>,{" "}
           <a href="mailto:alexandriabraniff@gmail.com" className="underline hover:opacity-80">
             alexandriabraniff@gmail.com
           </a>
